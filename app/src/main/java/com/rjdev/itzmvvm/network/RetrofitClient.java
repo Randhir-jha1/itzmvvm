3bb89c51fa0b52767dc3ab3c91fc96af99c0511b
Randhir Kumar Jha <jharandhir1994@gmail.com>
2020-09-04 22:01:19 +0530

    FirstCommit

--- app/src/main/java/com/rjdev/itzmvvm/network/RetrofitClient.java
+++ app/src/main/java/com/rjdev/itzmvvm/network/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://1.6.10.113/approve_api/webservices/index.php/";
+    private static final String BASE_URL = "YOUR_URL";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
